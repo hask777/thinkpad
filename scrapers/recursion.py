cfbae2f77255bbe4ed91d4d9581b35014fcc7eaa
hask777 <haskellisp@gmail.com>
2022-12-26 20:34:37 +0300

    f

--- scrapers/recursion.py
+++ scrapers/recursion.py
@@ -4,44 +4,53 @@ from bs4 import BeautifulSoup as bs
 
 main_url = 'https://cre-api-v2.kufar.by/items-search/v1/engine/v1/search/rendered-paginated?cat=16040&cursor=eyJ0IjoiYWJzIiwiZiI6dHJ1ZSwicCI6Mn0=&lang=ru&prn=16000&rgn=2&size=42&sort=lst.d'
 
-def data_get(url):
+def get_first():
+    url = 'https://cre-api-v2.kufar.by/items-search/v1/engine/v1/search/rendered-paginated?cat=16040&cursor=eyJ0IjoiYWJzIiwiZiI6dHJ1ZSwicCI6MX0=&lang=ru&prn=16000&rgn=2&size=42&sort=lst.d'
     response = requests.get(url).json()
+    pages = response.get("pagination").get("pages")
+    print(pages)
+
+
+    with open(f'files/main.json', 'w', encoding='utf-8') as f:
+        json.dump(response, f, indent=4, ensure_ascii=False)
+
+
+def data_get(url):
     totals = []
+
+    response = requests.get(url).json()
+
     pages = response.get("pagination").get("pages")[3].get("num")
     token = response.get("pagination").get("pages")[2].get("token")
-    print(token)
-
-    url = f'https://cre-api-v2.kufar.by/items-search/v1/engine/v1/search/rendered-paginated?cat=16040&cursor={token}&lang=ru&prn=16000&rgn=2&size=42&sort=lst.d'
-    count = 0
 
     #  we need function thst get url and return the nxt page token
     #  then we add this token to the url, to get ne token
-    def get_token(token, count):
-        count += 1
+    def get_token(token):
         next_page = requests.get(f'https://cre-api-v2.kufar.by/items-search/v1/engine/v1/search/rendered-paginated?cat=16040&cursor={token}&lang=ru&prn=16000&rgn=2&size=42&sort=lst.d').json()
         pagination = next_page.get("pagination").get("pages")
+        print(pagination)
 
         for page in pagination:
+            if page["label"] == 'self':
+                num = page["num"]
             # print(page['label'])
             if page['label'] == 'next':
                 token = page['token']
-                print(token)
+                
 
                 next_page = requests.get(f'https://cre-api-v2.kufar.by/items-search/v1/engine/v1/search/rendered-paginated?cat=16040&cursor={token}&lang=ru&prn=16000&rgn=2&size=42&sort=lst.d').json()
                 totals.append(next_page)
 
-                with open(f'files/page{count}.json', 'w', encoding='utf-8') as f:
+                with open(f'files/page{num}.json', 'w', encoding='utf-8') as f:
                     json.dump(next_page, f, indent=4, ensure_ascii=False)
 
-                if token is None:
-                    return  
+        return get_token(token) 
 
-        return get_token(token, count) 
-
-    get_token(token, count)
+    get_token(token)
 
 def main():
-    data_get(main_url)
+    get_first()
+    # data_get(main_url)
 
 if __name__ == '__main__':
     main()
